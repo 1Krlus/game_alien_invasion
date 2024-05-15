cd7d15a7e47603f766521acfeac6365517332cf0
unknown <cgsacad@gmail.com>
2024-05-14 22:12:30 -0300

    288

---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings():
+# Uma classe para armazenar todas as configurações do jogo
+    def __init__(self): # Inicializa as configurações do jogo
+        # Configurações da tela
+        self.screen_width = 1280
+        self.screen_height = 720
+        self.bg_color = (230, 230, 230) # Definindo a cor do fundo
---
+++ ship.py
@@ -0,0 +1,17 @@
+import pygame
+
+class Ship():
+    # Inicializa a espaço nave e define sua posição inicial
+    def __init__(self, screen):
+        self.screen = screen
+        # Carrega a imagem da espaçonave e obtém seu rect
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+        # Inicia cada nova espaçonave na parte inferior central da tela
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+    
+    # Desenha a espaçonave em sua posição atual.
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
